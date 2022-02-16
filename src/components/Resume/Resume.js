1eb31a9291a4869aa512bd63ea6b1a9030b620d8
mspronesti <massimiliano.pronesti@gmail.com>
2022-02-16 12:45:00 +0100

    feat: add cv

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../assets/Soumyajit-Behera.pdf";
+import pdf from "../../assets/pronesti_cv.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/mspronesti/portfolio/master/src/assets/pronesti_cv.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
