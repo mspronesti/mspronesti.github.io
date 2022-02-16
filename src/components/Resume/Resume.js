5851d28ba2257735e1aa1c9de4aa97404f376fe8
mspronesti <massimiliano.pronesti@gmail.com>
2022-02-16 12:55:52 +0100

    [cv] fix resume path

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/mspronesti/portfolio/master/src/assets/pronesti_cv.pdf";
+  "https://raw.githubusercontent.com/mspronesti/mspronesti.github.io/master/src/assets/pronesti_cv.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
